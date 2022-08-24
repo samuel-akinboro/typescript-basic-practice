1ee6b6f23bece5ed7232106c792ee108c281ba3b
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-08-24 18:02:37 +0100

    declaring data type in function parameters

--- sandbox.js
+++ sandbox.js
@@ -3,3 +3,5 @@ var character = 'ben';
   re-assigning a new value to the variable character
   that is not a type of string will throw an error
 */
+// declaring the data type of a parameter in functions
+var multiply = function (val) { return val * Math.PI; };
--- sandbox.ts
+++ sandbox.ts
@@ -3,3 +3,7 @@ let character = 'ben';
   re-assigning a new value to the variable character 
   that is not a type of string will throw an error
 */
+
+// declaring the data type of a parameter in functions
+
+const multiply = (val: number) => val * Math.PI
