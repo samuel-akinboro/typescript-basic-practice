93ebe136092e99ec2d807d4e7a40b5f5b146df66
Samuel Akinboro <victorakinboro2003@gmail.com>
2022-08-28 18:08:49 +0100

    classes

--- public/app.js
+++ public/app.js
@@ -1,4 +1,20 @@
 "use strict";
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes \$${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('bayo', 'bandek blog', 300);
+let invoices = [];
+invoices.push(invOne);
+console.log(invOne.format());
+// DOM
 const form = document.querySelector('.new-item-form');
 // inputs
 const type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c
+    this.details = d
+    this.amount = a
+  }
+
+  format() {
+    return `${this.client} owes \$${this.amount} for ${this.details}`
+  }
+}
+
+const invOne = new Invoice('bayo', 'bandek blog', 300)
+
+let invoices: Invoice[] = [];
+invoices.push(invOne)
+console.log(invOne.format())
+
+// DOM
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 // inputs
